75775601100e4f77f9209ba3bd5af6697968a30f
Derrick <547791743@qq.com>
2020-09-18 22:41:20 +0800

    feat: first commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from './react-redux'
+import { createStore, applyMiddleware } from './redux'
+import rootReducer from './reducers'
+
+// middleware
+const logger = (store:any) => (next:any) => (action:any) => {
+  // debugger
+  console.info('dispatching', action)
+  let result = next(action);
+  console.log('next state', store.getState())
+  return result
+}
+
+let store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/utils/isPlainObject.ts
@@ -0,0 +1,15 @@
+/**
+ * @param obj The object to inspect.
+ * @returns True if the argument appears to be a plain object.
+ */
+// 判断是否是对象
+export default function isPlainObject(obj: any): boolean {
+  if (typeof obj !== 'object' || obj === null) return false
+
+  let proto = obj
+  while (Object.getPrototypeOf(proto) !== null) {
+    proto = Object.getPrototypeOf(proto)
+  }
+
+  return Object.getPrototypeOf(obj) === proto
+}
